5aa4384ecf4dc9b55551cfc56bc85452729b0160
Cedric <cedric.roxas@gmail.com>
2015-09-22 21:18:40 +1000

    add test for time duration in hours

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -37,6 +37,13 @@ RSpec.feature "User visits event", :type => :feature do
     Event.create(time: 2.days.since, name: "destructive steps", description: "asdasdasd", dance_styles: [DanceStyle.new(name: "Locking"), DanceStyle.new(name: "Popping")], host: "Dancekool")
     visit "/events/1"
 
-  	expect(page).to have_text("Dancekool")
+    expect(page).to have_text("Dancekool")
+  end  
+
+  scenario "User sees time duration in hours" do
+    Event.create(time: Time.zone.parse('2015-08-22 18:00'), end_time: Time.zone.parse('2015-08-22 21:00'), name: "destructive steps", description: "asdasdasd", dance_styles: [DanceStyle.new(name: "Locking"), DanceStyle.new(name: "Popping")], host: "Dancekool")
+    visit "/events/1"
+
+  	expect(page).to have_text("Saturday 22 August at 6:00pm - 9:00pm")
   end
 end
