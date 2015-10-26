86b02195384323921f45140d768d5018ede64d37
MJ <mark.a@serviceseeking.com.au>
2015-10-26 21:37:03 +1100

    fix migrations

--- db/migrate/20150512120600_create_dance_classes.rb
+++ db/migrate/20150512120600_create_dance_classes.rb
@@ -3,7 +3,7 @@ class CreateDanceClasses < ActiveRecord::Migration
     create_table :dance_classes do |t|
       t.time :time
       t.text :description
-      t.references :dance_style, index: true, foreign_key: true
+      # t.references :dance_style, index: true, foreign_key: true
       t.references :teacher, index: true, foreign_key: true
 
       t.timestamps null: false
