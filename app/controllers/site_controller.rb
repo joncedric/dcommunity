571504359a98fcc017db725a30ab8dfd28d0520d
MJ <mark.a@serviceseeking.com.au>
2015-10-26 22:02:35 +1100

    fix upcoming events logic

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,6 @@
 class SiteController < ApplicationController
 	def index
 		@dance_styles = DanceStyle.top_styles
-    # @upcoming_events = Event.where('time >= ?', Time.now)
-		@upcoming_events = Event.all
+    @upcoming_events = Event.where('time >= ?', Time.now)
 	end
 end
