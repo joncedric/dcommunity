902a725ee571bdb279193219af7d7602f60530b9
MJ <mark.a@serviceseeking.com.au>
2015-11-09 22:12:39 +1100

    add Amazon s3 support for paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20151011084954) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dance_classes", force: :cascade do |t|
     t.time     "time"
-    t.text     "description",    limit: 65535
-    t.integer  "dance_style_id", limit: 4
-    t.integer  "teacher_id",     limit: 4
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.text     "description"
+    t.integer  "dance_style_id"
+    t.integer  "teacher_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   add_index "dance_classes", ["dance_style_id"], name: "index_dance_classes_on_dance_style_id", using: :btree
@@ -27,14 +30,14 @@ ActiveRecord::Schema.define(version: 20151011084954) do
 
   create_table "dance_styles", force: :cascade do |t|
     t.string   "name",        limit: 255
-    t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.text     "description"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "dance_styles_events", force: :cascade do |t|
-    t.integer "event_id",       limit: 4
-    t.integer "dance_style_id", limit: 4
+    t.integer "event_id"
+    t.integer "dance_style_id"
   end
 
   add_index "dance_styles_events", ["dance_style_id"], name: "index_dance_styles_events_on_dance_style_id", using: :btree
@@ -43,19 +46,19 @@ ActiveRecord::Schema.define(version: 20151011084954) do
   create_table "events", force: :cascade do |t|
     t.string   "name",                     limit: 255
     t.datetime "time"
-    t.text     "description",              limit: 65535
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.text     "description"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "flyer_image_file_name",    limit: 255
     t.string   "flyer_image_content_type", limit: 255
-    t.integer  "flyer_image_file_size",    limit: 4
+    t.integer  "flyer_image_file_size"
     t.datetime "flyer_image_updated_at"
     t.string   "location",                 limit: 255
     t.string   "cost",                     limit: 255
     t.string   "host",                     limit: 255
     t.datetime "end_time"
-    t.float    "latitude",                 limit: 24
-    t.float    "longitude",                limit: 24
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "studios", force: :cascade do |t|
