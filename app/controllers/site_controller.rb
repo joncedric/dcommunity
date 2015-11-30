1a6a6aeb7d329e84ff21472702c8a92be63d04d7
MJ <mark.a@serviceseeking.com.au>
2015-11-30 21:35:15 +1100

    sort upcoming events by time

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,6 @@
 class SiteController < ApplicationController
 	def index
 		@dance_styles = DanceStyle.top_styles
-    @upcoming_events = Event.where('time >= ?', Time.now)
+    @upcoming_events = Event.where('time >= ?', Time.now).sort_by(&:time)
 	end
 end
